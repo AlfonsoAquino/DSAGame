c1928176b051e3bbbb071cf7d344b980cfe5da25
alfon <alfonso.aquino.1996@gmail.com>
2019-04-14 16:16:32 +0200

    .

--- app/src/main/java/com/example/alfonsoaquino/test/SendFile.java
+++ app/src/main/java/com/example/alfonsoaquino/test/SendFile.java
@@ -40,7 +40,7 @@ public class SendFile extends AsyncTask<File, Void, String> {
             if (sourceFile.isFile()) {
 
                 try {
-                    String upLoadServerUri = "https://bifurun.000webhostapp.com/fileUp.php";
+                    String upLoadServerUri = Config.FILEUP;
 
                     // open a URL connection to the Servlet
                     FileInputStream fileInputStream = new FileInputStream(
